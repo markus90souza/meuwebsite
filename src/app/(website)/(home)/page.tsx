a144d14938a125cc04d3917f4785d43d44866441
rockske8er <rockske8er@gmail.com>
2024-05-19 02:03:39 -0300

    feat: create a hero section from homepage

--- src/app/(website)/(home)/page.tsx
+++ src/app/(website)/(home)/page.tsx
@@ -1,3 +1,51 @@
-export default function Home() {
-  return <h1>home</h1>
+import { Photo } from '@/components/photo'
+import { Socials } from '@/components/socials'
+import { Button } from '@/components/ui/button'
+import { DownloadIcon } from 'lucide-react'
+
+const HomePage = () => {
+  return (
+    <section className="h-full">
+      <div className="h-full container mx-auto">
+        <div className="flex flex-col items-center justify-between xl:flex-row xl:pb-24 pt-8">
+          <header className="text-center xl:text-left order-2 xl:order-none">
+            <strong className="text-xl">Frontend | Mobile Developer</strong>
+            <h1 className="title mb-6">
+              Olá eu sou <br />{' '}
+              <span className="text-app-accent subtitle">Marcos J.S</span>
+            </h1>
+
+            <p className="max-w-[500px] text-white/80 mb-9">
+              Frontend | Mobile Developer, apainado por tecnologia, busco sempre
+              me aprimorar e me tornar um desenvolvedor melhor, estou aprendendo
+              as tecnologias mais novas.
+            </p>
+
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                size={'lg'}
+                variant={'outline'}
+                className="bg-transparent flex items-center gap-2 border-app-accent rounded-full hover:text-app hover:bg-app-accent-hover"
+              >
+                <span className="text-app-accent hover:text-app">
+                  Baixar CV
+                </span>
+                <DownloadIcon className="w-5 h-5 text-app-accent" />
+              </Button>
+
+              <div className="mb-8 xl:mb-0">
+                <Socials />
+              </div>
+            </div>
+          </header>
+
+          <div className="order-1 mb-8 xl:mb-0 xl:order-none">
+            <Photo />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
+
+export default HomePage
---
+++ src/components/photo.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { motion } from 'framer-motion'
+import Image from 'next/image'
+
+import x from '@/assets/photo.png'
+export const Photo = () => {
+  return (
+    <div className='h-full w-full relative'>
+      
+      <motion.div 
+        initial={{ opacity: 0 }} 
+        animate={{ 
+          opacity: 1,
+          transition: { 
+            duration: 0.4,
+            delay: 1,
+            ease: 'easeIn'
+          } 
+        }}>
+        <motion.div 
+           initial={{ opacity: 0 }} 
+           animate={{ 
+             opacity: 1,
+             transition: { 
+               duration: 0.4,
+               delay: 1.4,
+               ease: 'easeInOut'
+             } 
+           }}
+        
+        className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten">
+          <Image src={x} 
+            alt="photo" 
+            priority
+            quality={100}
+            fill
+            className='object-contain'
+         />
+        </motion.div>  
+      </motion.div>  
+    </div>
+  )
+}
